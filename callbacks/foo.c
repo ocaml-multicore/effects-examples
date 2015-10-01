212c04585de00a4a56e4503c536d00646117726f
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2015-10-01 08:22:26 +0100

    Tabs to spaces

--- callbacks/foo.c
+++ callbacks/foo.c
@@ -3,14 +3,14 @@
 #include <caml/memory.h>
 #include <caml/callback.h>
 
-value caml_to_c	(value unit) {
-	CAMLparam1 (unit);
-	printf ("[C] Enter caml_to_c\n");
+value caml_to_c (value unit) {
+  CAMLparam1 (unit);
+  printf ("[C] Enter caml_to_c\n");
 
 #ifdef MULTI
-	static value c_to_caml_closure;
+  static value c_to_caml_closure;
   static int found = 0;
-	if (!found) {
+  if (!found) {
     c_to_caml_closure = caml_get_named_value ("c_to_caml", &found);
     if (!found) {
       printf ("caml_to_c: c_to_caml closure not found\n");
@@ -18,18 +18,18 @@ value caml_to_c	(value unit) {
     }
   }
 
-	printf ("[C] Call c_to_caml\n");
-	caml_callback(c_to_caml_closure, Val_unit);
+  printf ("[C] Call c_to_caml\n");
+  caml_callback(c_to_caml_closure, Val_unit);
 #else
-	static value * c_to_caml_closure = NULL;
-	if (c_to_caml_closure == NULL)
-		c_to_caml_closure = caml_named_value("c_to_caml");
+  static value * c_to_caml_closure = NULL;
+  if (c_to_caml_closure == NULL)
+    c_to_caml_closure = caml_named_value("c_to_caml");
 
-	printf ("[C] Call c_to_caml\n");
-	caml_callback(*c_to_caml_closure, Val_unit);
+  printf ("[C] Call c_to_caml\n");
+  caml_callback(*c_to_caml_closure, Val_unit);
 #endif
-	printf ("[C] Return from c_to_caml\n");
+  printf ("[C] Return from c_to_caml\n");
 
-	printf ("[C] Leave caml_to_c\n");
-	CAMLreturn (Val_unit);
+  printf ("[C] Leave caml_to_c\n");
+  CAMLreturn (Val_unit);
 }
