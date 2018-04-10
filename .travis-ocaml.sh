3a7a232981b88248e500d2875df6ada2db3ba1cc
KC Sivaramakrishnan <kc@kcsrk.info>
2018-04-10 12:11:57 +0100

    Edit

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -121,11 +121,11 @@ install_on_osx () {
   brew update &> /dev/null
   case "$OCAML_VERSION,$OPAM_VERSION" in
     4.02.2+multicore,1.2.2) OCAML_VERSION=4.02; OCAML_FULL_VERSION=4.02.2+multicore
-			brew install opam ;;
+			brew install --force opam ;;
     4.04.2+multicore,1.2.2) OCAML_VERSION=4.02; OCAML_FULL_VERSION=4.04.2+multicore
-			brew install opam ;;
+			brew install --force opam ;;
     4.06.1+multicore,1.2.2) OCAML_VERSION=4.02; OCAML_FULL_VERSION=4.06.1+multicore
-			brew install opam ;;
+			brew install --force opam ;;
     *) echo "Unknown OCAML_VERSION=$OCAML_VERSION OPAM_VERSION=$OPAM_VERSION"
        exit 1 ;;
   esac
