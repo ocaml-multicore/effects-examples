707e5c0e0a262e2a48554f8d832b3ec0cf5807f6
KC Sivaramakrishnan <kc@kcsrk.info>
2020-12-02 11:13:01 +0530

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -22,7 +22,7 @@ sh .travis-ocaml.sh
 export OPAMYES=1
 eval $(opam config env)
 
-opam switch create 4.10.0+multicore --packages=ocaml-variants.4.10.0+multicore,ocaml-secondary-compiler --repositories=multicore=git+https://github.com/ocamllabs/multicore-opam.git,default
+opam switch create 4.10.0+multicore --packages=ocaml-variants.4.10.0+multicore --repositories=multicore=git+https://github.com/ocamllabs/multicore-opam.git,default
 eval $(opam config env)
 opam install ocamlfind ocamlbuild
 cd $REPO_DIR
