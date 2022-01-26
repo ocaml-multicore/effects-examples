1ace5b9903d8b2b04f4b99579f6c14ddde6e2943
Patrick Ferris <patrick@sirref.org>
2022-01-26 08:46:58 +0000

    Add queens example from effects benchmarks

--- multishot/queens.ml
+++ multishot/queens.ml
@@ -1,59 +1,60 @@
 open Effect
 open Effect.Deep
 
-type _ eff += Select : 'a list -> 'a eff
+(* Ported from: https://github.com/effect-handlers/effect-handlers-bench/blob/d4a32ec337b77859c328a1103e195d3e4b3dcb5b/benchmarks/ocaml/001_nqueens/001_nqueens_ocaml.ml *)
+let n = try int_of_string Sys.argv.(1) with _ -> 8
 
-let rec filter p = function
-  | [] -> []
-  | x :: xs ->
-    if p x then (x :: filter p xs) else filter p xs
-
-let rec forall p = function
+let rec safe queen diag xs =
+  match xs with
   | [] -> true
-  | x :: xs -> if p x then forall p xs else false
+  | q :: qs -> queen <> q && queen <> q + diag && queen <> q - diag &&
+               safe queen (diag + 1) qs
 
-let no_attack (x,y) (x',y') =
-  x <> x' && y <> y' && abs (x - x') <> abs (y - y')
+type _ eff += Pick : int -> int eff
+exception Fail
 
-let available x qs l =
-  filter (fun y -> forall (no_attack (x,y)) qs) l
+let rec find_solution n col : int list =
+  if col = 0 then []
+  else begin
+    let sol = find_solution n (col - 1) in
+    let queen = perform (Pick n) in
+    if safe queen 1 sol then queen::sol else raise Fail
+  end
 
-let find_solution n =
-  let run n =
-    let l = ref [] in
-    for i = n downto 1 do
-      l := i::!l;
-    done;
-    let rec place x qs =
-      if x = n+1 then Some qs else
-        let y = perform @@ Select (available x qs !l) in
-        place (x+1) ((x, y) :: qs)
-    in place 1 []
-  in
-  try_with run n {
+let queens_count n =
+  match_with (find_solution n) n {
+    retc = (fun _ -> 1);
+    exnc = (function Fail -> 0 | e -> raise e);
     effc = fun (type a) (e : a eff) ->
       match e with
-      | Select lst -> Some (fun k ->
-        let rec loop = function
-          | [] -> None
-          | x::xs ->
-              match continue (Multicont.Deep.clone_continuation k) x with
-              | None -> loop xs
-              | Some x -> Some x
-        in loop lst)
+      | Pick n -> Some (fun (k : (a, _) continuation) ->
+        let rec loop i acc =
+          if i = n then (continue k i + acc)
+          else loop (i+1) (continue (Multicont.Deep.clone_continuation k) i + acc)
+        in loop 1 0)
       | _ -> None
   }
 
-let main () =
-  let n =
-    if Array.length Sys.argv > 1
-    then int_of_string Sys.argv.(1)
-    else 8
-  in
-  match find_solution n with
-  | None -> print_endline "No solutions found"
-  | Some l ->
-      List.iter (fun (x,y) -> Printf.printf "(%d,%d) " x y) l;
-      print_endline ""
+let queens_choose n =
+  match_with (find_solution n) n {
+    retc = (fun x -> [ x ]);
+    exnc = (function Fail -> [] | e -> raise e);
+    effc = fun (type a) (e : a eff) ->
+      match e with
+      | Pick n -> Some (fun (k : (a, _) continuation) ->
+        let rec loop i acc : int list list =
+          if i = 1 then (continue k i @ acc)
+          else loop (i-1) (continue (Multicont.Deep.clone_continuation k) i @ acc)
+        in loop n [])
+      | _ -> None
+  }
 
-let () = main ()
+let print_all_solutions () =
+  let sols = queens_choose n in
+  List.iter (fun l ->
+    List.iter (fun pos -> Printf.printf "%d " pos) l;
+    print_endline "") sols
+
+let _ =
+  print_all_solutions ();
+  Printf.printf "%d\n" (queens_count n)
